f421fe14a62108a28abd7265aec7220b66fe9634
Johann-Michael1 <>
2022-01-11 15:15:14 +0000

    Add basic setup

---
+++ src/main.spec.ts
@@ -0,0 +1,9 @@
+import {hello} from './main'
+
+describe("hello()", () => {
+  it("should return 'Hello, Meticulous!'", () => {
+    const result = hello();
+
+    expect(result).toBe("Hello, Meticulous!");
+  })
+})
